67c6165a2f82646975cfea019b95e9ec82cb06e4
Debabrata Ghosh <avroghosh33@gmail.com>
2020-04-24 17:59:42 +0530

    test commit

--- src/com/basic/Main.java
+++ src/com/basic/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello World hello");
+        System.out.print("Hello World hello hi");
 
     }
 }
