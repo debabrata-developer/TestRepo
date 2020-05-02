270ea0a25801d7a23d6c6ba28ca970c675001b88
Debabrata Ghosh <avroghosh33@gmail.com>
2020-05-02 22:53:43 +0530

    commit by DG

--- src/com/basic/Testing.java
+++ src/com/basic/Testing.java
@@ -4,19 +4,20 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class CommonClass {
-
+public class Testing {
     static WebDriver driver;
     // Read Excel File
     File src = new File("C:\\AMIGO Selenium Excel Sheet.xlsx");
@@ -24,11 +25,12 @@ public class CommonClass {
     XSSFWorkbook workbook = new XSSFWorkbook(input);
     XSSFSheet sheet = workbook.getSheetAt(0);
 
-    public CommonClass() throws IOException {
+    public Testing() throws IOException {
     }
 
     @BeforeTest
-    public void Setup() throws IOException {
+    public void Setup(){
+
         //get WebDriver Path
         String webDriverPath = sheet.getRow(3).getCell(2).getStringCellValue();
         System.out.println(webDriverPath);
@@ -71,6 +73,60 @@ public class CommonClass {
     @Test(priority = 1)
     public void testMethod(){
         System.out.println("it is test method");
+    }
+
+    @Test(priority = 2)
+    public void CreateOrganization() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+
+        //click New Button
+        WebElement myDynamicElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@title=\"New\" and text()='New']")));
+        myDynamicElement.click();
+
+        //Organization Name
+        myDynamicElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div/input[@class=\" input\" and @type=\"text\"]")));
+        myDynamicElement.sendKeys("Selenium Test Organization");
+
+        //Timesheet Auto Approval Days
+        driver.findElement(By.xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/article/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/input")).sendKeys("7");
+
+        //Expense Auto Approval Days
+        driver.findElement(By.xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/article/div[3]/div/div[2]/div/div/div/div[2]/div/div/div/input")).sendKeys("9");//(//div/input[@class="input uiInputSmartNumber"  and @data-aura-class="uiInputSmartNumber"])[2]
+
+        //Historical Comments
+        driver.findElement(By.xpath("//div[@class=\"ql-editor ql-blank slds-rich-text-area__content slds-text-color_weak slds-grow\" ]")).sendKeys("Selenium Test Historical Comment");
+
+        //Click Save Button
+        driver.findElement(By.xpath("//button[@title=\"Save\" ]//span[text()='Save']")).click();
+
+        Thread.sleep(5000);
+    }
+
+    @Test(priority = 3)
+    public void EditOrganization() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+
+        //Click Edit Button
+        WebElement myDynamicElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@title=\"Edit\"]//div[text()='Edit']")));
+        myDynamicElement.click();
+
+        //Organization Name
+        myDynamicElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div/input[@class=\" input\" and @type=\"text\"]")));
+        myDynamicElement.sendKeys(" Edit");
+
+        //Expense Auto Approval Days
+        driver.findElement(By.xpath("(//div/input[@class=\"input uiInputSmartNumber\"  and @data-aura-class=\"uiInputSmartNumber\"])[2]")).sendKeys("9");
+
+        //Timesheet Auto Approval Days
+        driver.findElement(By.xpath("(//div/input[@class=\"input uiInputSmartNumber\"  and @data-aura-class=\"uiInputSmartNumber\"])[1]")).sendKeys("1");
+
+        //Historical Comments
+        driver.findElement(By.xpath("//div[@class=\"ql-editor ql-blank slds-rich-text-area__content slds-text-color_weak slds-grow\" ]")).sendKeys("Selenium Test Historical Comment Edit");
+
+        //Click Save Button
+        driver.findElement(By.xpath("//button[@title=\"Save\" ]//span[text()='Save']")).click();
+
+        Thread.sleep(5000);
     }
 
     @AfterTest
