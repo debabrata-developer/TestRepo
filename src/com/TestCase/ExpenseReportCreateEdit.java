67c279293dacec4bd5f452a7958de4e47d709490
Sukanya <sp@techcloudsolutions.in>
2020-06-06 15:08:28 +0530

    Commit By Sukanya

--- src/com/TestCase/ExpenseReportCreateEdit.java
+++ src/com/TestCase/ExpenseReportCreateEdit.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -251,16 +252,11 @@ public class ExpenseReportCreateEdit {
         Thread.sleep(5000);
 
 
-
-
-
-
-
-
-
-
-
     }
-
+    @AfterTest
+    public void close() {
+        //closing the chrome
+        driver.quit();
+    }
 
 }
